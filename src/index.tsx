6b9986f4d14084a8cb6da9d6b9b7cbd178a397f5
andregom <andre.oliveira@ccc.ufcg.edu.br>
2022-01-28 13:27:53 -0300

    removes outlet

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Outlet } from 'react-router-dom';
 
 import GlobalStyles from './styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
@@ -14,7 +13,6 @@ ReactDOM.render(
   <ThemeProvider theme={dark}>
     <React.StrictMode>
       <GlobalStyles />
-      <Outlet />
       <ContentRoutes />
     </React.StrictMode>
   </ThemeProvider>,
